1505b59462db3d59e1fb0ec4d2fbef21c5231399
Nikita <matveevnikita@yahoo.com>
2020-06-14 19:03:47 +0300

    Deleted lesson 1

--- ex_1.py
+++
@@ -1,8 +0,0 @@
-print('hello')
-print('what is the day today (dd.mm.yyyy)?')
-current_date = input()
-q_days = int(current_date[0:2]) + (int(current_date[3:5]) - 3)*30
-print('days on quarantine:', q_days)
-print('when were you born?')
-city = input()
-print('in', city, '100 days in quarantine more')
