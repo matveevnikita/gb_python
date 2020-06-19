afa066ed5b2dadc6d6fc6da0e0618555bcb2843c
Nikita <matveevnikita@yahoo.com>
2020-06-19 11:41:18 +0300

    Ex5 correction

--- l2_ex5.py
+++ l2_ex5.py
@@ -9,17 +9,30 @@
 # Набор натуральных чисел можно задать непосредственно в коде, например,
 # my_list = [7, 5, 3, 3, 2].
 
+# my_list = [7, 5, 3, 3, 2]
+# print('the rating is: ', my_list)
+# print('enter new number: ')
+# n = int(input())
+# pos = 0
+# if n < my_list[len(my_list)-1]:
+#     my_list.append(n)
+# else:
+#     for i in my_list:
+#         if n >= i:
+#             my_list.insert(pos,n)
+#             break
+#         pos = pos + 1
+# print('the new rating is: ', my_list)
+
+# Task 5
 my_list = [7, 5, 3, 3, 2]
-print('the rating is: ', my_list)
-print('enter new number: ')
-n = int(input())
-pos = 0
-if n < my_list[len(my_list)-1]:
-    my_list.append(n)
+request = input('Введите число: ')
+for index, number in enumerate(my_list):
+    if int(request) < int(number):
+        continue
+    my_list.insert(index, request)
+    print(my_list)
+    break
 else:
-    for i in my_list:
-        if n >= i:
-            my_list.insert(pos,n)
-            break
-        pos = pos + 1
-print('the new rating is: ', my_list)
+    my_list.append(request)
+    print(my_list)
